db00ed3d0c41fcab25d1db14b42b18454c12ddc0
yangguangzb <2570886020@qq.com>
2017-12-06 17:59:43 +0800

    问题servlet

--- src/com/san/servlet/QuestionServlet.java
+++ src/com/san/servlet/QuestionServlet.java
@@ -6,8 +6,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.san.model.User;
+import com.san.service.Impl.IntegralServiceImpl;
+
 public class QuestionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	IntegralServiceImpl integralServiceImpl=new IntegralServiceImpl();
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		if(request.getParameter("flag")!=null){
@@ -20,6 +24,14 @@ public class QuestionServlet extends HttpServlet {
 	}
 	public void putQuestion(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.getParameter("");
+		//获取课程名,问题积分,问题标题,问题内容
+		String courseName=request.getParameter("courseName");
+		String questionReward=request.getParameter("questionReward");
+		String questionTitle=request.getParameter("questionTitle");
+		String questionContent=request.getParameter("questionContent");
+		//判断用户提出问题设置积分是否够
+		User user=(User) request.getSession().getAttribute("user");
+		String i=integralServiceImpl.isQuestionPointsService(user.getUserId(),questionReward,questionTitle,questionContent,courseName)+"";
+		response.getWriter().write(i);
 	}
 }
