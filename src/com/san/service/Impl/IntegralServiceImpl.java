87a1e4f76e7a30ddc593397d43533297742866ab
yangguangzb <2570886020@qq.com>
2017-12-06 17:59:10 +0800

    问题积分与用户积分数据处理

--- src/com/san/service/Impl/IntegralServiceImpl.java
+++ src/com/san/service/Impl/IntegralServiceImpl.java
@@ -7,6 +7,7 @@ import com.san.dao.Impl.IntegralDaoImpl;
 public class IntegralServiceImpl {
 	//积分使用记录处理
 	IntegralDaoImpl integralDaoImpl=new IntegralDaoImpl();
+	QuestionServiceImpl questionServiceImpl=new QuestionServiceImpl();
 	/**
 	 * @param userId	用户编号
 	 * @param useType	使用类型
@@ -21,4 +22,22 @@ public class IntegralServiceImpl {
 		}
 		return 0;
 	}
+	//问题积分是否够
+	public int isQuestionPointsService(int userId,String questionReward,
+			String questionTitle,String questionContent,String courseName){
+		int intquestionReward=Integer.parseInt(questionReward);
+		try {
+			int i= integralDaoImpl.isQuestionPoints(userId, intquestionReward);
+			if(i==1){
+				//积分够用,插入问题信息
+				questionServiceImpl.insertQuestServiceImpl(userId, questionTitle, questionContent, questionReward, courseName);
+				return i;
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
 }
