9475c82662ea413074b8d8a116a2f1ba73c0f182
yangguangzb <2570886020@qq.com>
2017-12-07 20:55:24 +0800

    增加待回答问题处理

--- src/com/san/service/Impl/QuestionServiceImpl.java
+++ src/com/san/service/Impl/QuestionServiceImpl.java
@@ -1,6 +1,9 @@
 package com.san.service.Impl;
 import java.sql.SQLException;
+import java.util.List;
+
 import com.san.dao.Impl.QuestionDaoImpl;
+import com.san.model.Question;
 public class QuestionServiceImpl {
 	//插入一条问题记录
 	QuestionDaoImpl questionDaoImpl=new QuestionDaoImpl();
@@ -12,5 +15,14 @@ public class QuestionServiceImpl {
 			e.printStackTrace();
 		}
 		return 0;
+	}
+	//待回答问题service
+	public List<Question> notAnswerService(){
+		try {
+			return questionDaoImpl.notAnswerQuestionDaoImpl();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
