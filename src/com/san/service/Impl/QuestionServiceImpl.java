12f52568b2b68149389262e6a33a06345f78b941
yangguangzb <2570886020@qq.com>
2017-12-09 20:43:18 +0800

    添加网友答案等处理

--- src/com/san/service/Impl/QuestionServiceImpl.java
+++ src/com/san/service/Impl/QuestionServiceImpl.java
@@ -2,8 +2,12 @@ package com.san.service.Impl;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.junit.Test;
+
 import com.san.dao.Impl.QuestionDaoImpl;
+import com.san.model.Answer;
 import com.san.model.Question;
+import com.san.utils.DBUtil;
 public class QuestionServiceImpl {
 	//插入一条问题记录
 	QuestionDaoImpl questionDaoImpl=new QuestionDaoImpl();
@@ -17,9 +21,47 @@ public class QuestionServiceImpl {
 		return 0;
 	}
 	//待回答问题service
+	@Test
 	public List<Question> notAnswerService(){
 		try {
-			return questionDaoImpl.notAnswerQuestionDaoImpl();
+			List<Question> questions=questionDaoImpl.notAnswerQuestionDaoImpl();
+			for (Question question : questions) {
+				//将数据库中的时间转化
+				question.setShowTime(DBUtil.getTimeByDate(question.getQuestionCreationTime()));
+				//设置问题回答次数
+				int number=questionDaoImpl.answerNumber(question.getQuestionId());
+				question.setAnswerNumber(number);
+			}
+			return questions;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	//回答问题的service
+	public int answerQuestionService(String questionId,int userId,String answerContent){
+		try {
+			questionDaoImpl.answerQuestionDaoImpl(questionId, userId, answerContent);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	//显示网友回答的答案
+	public List<Answer> showAllAnswerService(String questionId){
+		try {
+			int intquestionId=Integer.parseInt(questionId);
+			return questionDaoImpl.showAllAnswerDaoImpl(intquestionId);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	//根据问题编号，查询问题信息
+	public Question questionByIdService(String questionId){
+		try {
+			int intquestionId=Integer.parseInt(questionId);
+			questionDaoImpl.questionByIdDaoImpl(intquestionId);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
