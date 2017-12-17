e634cb3fa1ff732a86ca78f3f92f7008b5048adf
yangguangzb <2570886020@qq.com>
2017-12-17 20:17:22 +0800

    积分servlet修改

--- src/com/san/servlet/IntegralServlet.java
+++ src/com/san/servlet/IntegralServlet.java
@@ -1,6 +1,9 @@
 package com.san.servlet;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -22,6 +25,16 @@ public class IntegralServlet extends HttpServlet {
 			}
 		}
 	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if(request.getParameter("flag")!=null){
+			String flag=request.getParameter("flag");
+			//后台查看积分
+			if(flag.equals("backCheckIntegral")){
+				backCheckIntegral(request, response);
+			}
+		}
+	}
 	public void buyPoints(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String buyNum=request.getParameter("buyNum");
@@ -31,5 +44,16 @@ public class IntegralServlet extends HttpServlet {
 		User user1=userServiceImpl.checkUserByIdService(user.getUserId());
 		request.getSession().setAttribute("user", user1);
 		response.getWriter().write(i+"");
+	}
+	//后台查询积分
+	public void backCheckIntegral(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String start=request.getParameter("start");
+		String end=request.getParameter("end");
+		String integralNumber=request.getParameter("integralNumber");
+		String username=request.getParameter("username");
+		List<Map<String, Object>> integralListMap= integralServiceImpl.checkIntegralService(start, end, integralNumber, username);
+		request.setAttribute("integralListMap", integralListMap);
+		request.getRequestDispatcher("admin2/integral-list.jsp").forward(request, response);
 	}
 }
