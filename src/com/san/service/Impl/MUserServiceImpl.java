8bbcb0fa31d65686b2dd96851cd6d38af80bb501
yangguangzb <2570886020@qq.com>
2017-12-18 17:29:40 +0800

    完善后台用户管理

--- src/com/san/service/Impl/MUserServiceImpl.java
+++ src/com/san/service/Impl/MUserServiceImpl.java
@@ -11,6 +11,7 @@ public class MUserServiceImpl {
 	UserDaoImpl userDaoImpl=new UserDaoImpl();
 	public List<User> mcheckUserService(String userType,String userId,String verification){
 		try {
+			System.out.println();
 			return userDaoImpl.mcheckUser(userType,userId,verification);
 		} catch (SQLException e) {
 			e.printStackTrace();
