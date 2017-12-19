22a5fa93bd778433a3577392d7189f19899aed0f
yangguangzb <2570886020@qq.com>
2017-12-19 21:35:18 +0800

    课程实体

---
+++ src/com/san/model/Course.java
@@ -0,0 +1,19 @@
+package com.san.model;
+
+public class Course {
+
+	private String courseName;//课程名
+	private String courseIntroduction;//课程描述
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public String getCourseIntroduction() {
+		return courseIntroduction;
+	}
+	public void setCourseIntroduction(String courseIntroduction) {
+		this.courseIntroduction = courseIntroduction;
+	}
+}
